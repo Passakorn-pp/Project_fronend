ad27f34b1985e78160033f430c03513c30360434
Passakorn <thejokerza11@gmail.com>
2020-07-09 14:29:05 +0700

    first commit

---
+++ test/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div >
+      <div v-for="(room,index) in form" :key="index">
+          {{room.name_place}}
+        
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: [
+          {
+          name_place: 'aaaa',
+          date: '',
+          radio: null,
+          selected: 'A',
+          active:false
+          },
+          {
+          name_place: 'bbbb',
+          date: '',
+          radio: null,
+          selected: 'B',
+          active:false
+          },
+        ],
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
